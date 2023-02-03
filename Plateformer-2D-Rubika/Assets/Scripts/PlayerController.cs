d7c2d99986da6353ba1587eb554c04788a69e14b
LeBud <charles.huyghe02@gmail.com>
2023-02-03 11:11:16 +0100

    PlayerController Update

--- Plateformer-2D-Rubika/Assets/Scripts/PlayerController.cs
+++ Plateformer-2D-Rubika/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float accelRate;
     [SerializeField] float deccelRate;
 
+    [Header("Jump Settings")]
+    [SerializeField] float jumpForce;
+    [SerializeField] float coyoteTime;
+    [SerializeField] float jumpInputBuffer;
+    [SerializeField] Transform groundCheckPos;
+    [SerializeField] Vector2 groundCheckSize;
+    [SerializeField] LayerMask groundCheckLayer;
+    bool isJumping;
+
+    float lastTimeOnGround;
+    float lastJumpPressed;
+
     Vector2 inputMov;
 
     private void Awake()
@@ -21,7 +33,14 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        lastTimeOnGround -= Time.deltaTime;
+        lastJumpPressed -= Time.deltaTime;
+
         MyInputs();
+
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundCheckLayer)) lastTimeOnGround = coyoteTime;
+
+        if (lastJumpPressed > 0 && lastTimeOnGround > 0) Jump();
     }
 
     void MyInputs()
@@ -29,6 +48,19 @@ public class PlayerController : MonoBehaviour
         inputMov.x = Input.GetAxisRaw("Horizontal");
         inputMov.y = Input.GetAxisRaw("Vertical");
 
+        if (Input.GetButtonDown("Jump")) lastJumpPressed = jumpInputBuffer;
+    }
+
+    void Jump()
+    {
+        lastTimeOnGround = 0;
+        lastJumpPressed = 0;
+
+        float force = jumpForce;
+        if (rb.velocity.y < 1)
+            force -= rb.velocity.y;
+
+        rb.AddForce(Vector2.up * force, ForceMode2D.Impulse);
     }
 
     private void FixedUpdate()
@@ -49,4 +81,12 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(appliedForce * Vector2.right, ForceMode2D.Force);
     }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+    }
+
 }
