6a2f7b623b032acd98b11cc23c7e66e40fabd059
LeBud <charles.huyghe02@gmail.com>
2023-02-28 11:20:54 +0100

    m

--- Plateformer-2D-Rubika/Assets/Scripts/AirFlow.cs
+++ Plateformer-2D-Rubika/Assets/Scripts/AirFlow.cs
@@ -12,17 +12,23 @@ public class AirFlow : MonoBehaviour
     Rigidbody2D rb;
     PlayerController playerController;
 
+    Quaternion angle;
+    Vector2 dir;
+
     private void Awake()
     {
         rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
         playerController = rb.transform.GetComponent<PlayerController>();
+
+        angle = Quaternion.AngleAxis(transform.rotation.z, Vector2.right);
+        dir = angle * transform.right;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && playerController.gliding)
         {
-            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.velocity = new Vector2(rb.velocity.x / 4, rb.velocity.y / 4);
         }
     }
 
@@ -30,9 +36,10 @@ public class AirFlow : MonoBehaviour
     {
         if (collision.CompareTag("Player") && playerController.gliding)
         {
+            playerController.glideTime = playerController.playerControllerData.maxGlideTime;
             playerController.inAirFlow = true;
-            rb.velocity = new Vector2(rb.velocity.x, airFlowForce);
-            //rb.AddForce(transform.right * airFlowForce, ForceMode2D.Force);
+
+            rb.velocity = new Vector2(dir.x * airFlowForce, dir.y * airFlowForce);
         }
     }
 
@@ -41,7 +48,6 @@ public class AirFlow : MonoBehaviour
         if (collision.CompareTag("Player") && playerController.gliding)
         {
             playerController.inAirFlow = false;
-            //rb.AddForce(transform.right * airFlowForce, ForceMode2D.Impulse);
         }
     }
 
