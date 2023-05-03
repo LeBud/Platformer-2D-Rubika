dec70d705b3d90634d9512217ac1e3cf8075cc13
LeBud <charles.huyghe02@gmail.com>
2023-05-03 17:31:48 +0200

    sqdg

--- Plateformer-2D-Rubika/Assets/Scripts/Achievement/AchievementsCheck.cs
+++ Plateformer-2D-Rubika/Assets/Scripts/Achievement/AchievementsCheck.cs
@@ -26,24 +26,6 @@ public class AchievementsCheck : MonoBehaviour
         InitializeAchievements();
     }
 
-    public bool AchievementUnlocked(string achievementName)
-    {
-        bool result = false;
-
-        if (achievements == null)
-            return false;
-
-        Achievement[] achievementsArray = achievements.ToArray();
-        Achievement a = Array.Find(achievementsArray, ach => achievementName == ach.title);
-
-        if (a == null)
-            return false;
-
-        result = a.achieved;
-
-        return result;
-    }
-
     private void InitializeAchievements()
     {
         if (achievements == null)
@@ -51,30 +33,31 @@ public class AchievementsCheck : MonoBehaviour
             achievements = new List<Achievement>
             {
                 //general Achievements
-                new Achievement("Jump", "Jump 100 time.", tempSprite, (object o) => (jumpCount >= 100)),
-                new Achievement("Jumping a lot !", "Jump 1000 time.", tempSprite, (object o) => (jumpCount >= 1000)),
-                new Achievement("First time ?", "Die for the 1st time.", tempSprite, (object o) => (deathCount >= 1)),
-                new Achievement("It's not that hard !", "Die 100 time.", tempSprite, (object o) => (deathCount >= 100)),
+                new Achievement("Jump", "Jump 100 time.", tempSprite, (AchievementsCheck o) => jumpCount >= 100),
+                new Achievement("Jumping a lot !", "Jump 1000 time.", tempSprite, (AchievementsCheck o) => jumpCount >= 1000),
+                new Achievement("First time ?", "Die for the 1st time.", tempSprite, (AchievementsCheck o) => deathCount >= 1),
+                new Achievement("It's not that hard !", "Die 100 time.", tempSprite, (AchievementsCheck o) => deathCount >= 100),
                 //collectables Achievements
-                new Achievement("Nice !", "Find your 1st collectables", tempSprite, (object o) => (collectableCount >= 1)),
-                new Achievement("Moooore !", "Find half of the collectables", tempSprite, (object o) => (collectableCount >= 5)),
-                new Achievement("All of them !", "Find all the collectables", tempSprite, (object o) => (collectableCount >= 10)),
+                new Achievement("Nice !", "Find your 1st collectables", tempSprite, (AchievementsCheck o) => collectableCount >= 1),
+                new Achievement("Moooore !", "Find half of the collectables", tempSprite, (AchievementsCheck o) => collectableCount >= 5),
+                new Achievement("All of them !", "Find all the collectables", tempSprite, (AchievementsCheck o) => collectableCount >= 10),
                 //gamplay Achievments
-                new Achievement("Flying !", "Get in an aitflow for the 1st time.", tempSprite, (object o) => (airFlowUse == true)),
-                new Achievement("Bouncy", "Bounce on a jump pad fort the 1st time.", tempSprite, (object o) => (jumpPadUse == true)),
-                new Achievement("Too heavy !", "Bounce on a jump pad fort the 1st time.", tempSprite, (object o) => (breakPlatformUse == true)),
-                new Achievement("Lumios !", "Bounce on a jump pad fort the 1st time.", tempSprite, (object o) => (aphidUse == true)),
-                new Achievement("Watch out !", "Bounce on a jump pad fort the 1st time.", tempSprite, (object o) => (fallObjectUse == true)),
+                new Achievement("Flying !", "Get in an aitflow for the 1st time.", tempSprite, (AchievementsCheck o) => airFlowUse == true),
+                new Achievement("Bouncy", "Bounce on a jump pad fort the 1st time.", tempSprite, (AchievementsCheck o) => jumpPadUse == true),
+                new Achievement("Too heavy !", "Bounce on a jump pad fort the 1st time.", tempSprite, (AchievementsCheck o) => breakPlatformUse == true),
+                new Achievement("Lumios !", "Bounce on a jump pad fort the 1st time.", tempSprite, (AchievementsCheck o) => aphidUse == true),
+                new Achievement("Watch out !", "Bounce on a jump pad fort the 1st time.", tempSprite, (AchievementsCheck o) => fallObjectUse == true),
                 //story Achivements
-                new Achievement("How did we get here ?", "Bounce on a jump pad fort the 1st time.", tempSprite, (object o) => (fallInCatacomb == true)),
-                new Achievement("Better luck next time !", "Bounce on a jump pad fort the 1st time.", tempSprite, (object o) => (surviveChase == true)),
-                new Achievement("Out of there !", "Bounce on a jump pad fort the 1st time.", tempSprite, (object o) => (exitCatacomb == true)),
-                new Achievement("Congratulations !", "Bounce on a jump pad fort the 1st time.", tempSprite, (object o) => (endGame == true))
+                new Achievement("How did we get here ?", "Bounce on a jump pad fort the 1st time.", tempSprite, (AchievementsCheck o) => fallInCatacomb == true),
+                new Achievement("Better luck next time !", "Bounce on a jump pad fort the 1st time.", tempSprite, (AchievementsCheck o) => surviveChase == true),
+                new Achievement("Out of there !", "Bounce on a jump pad fort the 1st time.", tempSprite, (AchievementsCheck o) => exitCatacomb == true),
+                new Achievement("Congratulations !", "Bounce on a jump pad fort the 1st time.", tempSprite, (AchievementsCheck o) => endGame == true)
             };
         }
 
         foreach(Achievement achi in achievements)
         {
+            achi.achievementsCheck = this;
             achi.saveSystem = saveSystem;
             achi.display = display;
         }
@@ -133,7 +116,7 @@ public class Achievement
     public SaveSystem saveSystem;
     public AchievementsDisplay display;
 
-    public Achievement(string title, string description, Sprite sprite, Predicate<object> requirement) 
+    public Achievement(string title, string description, Sprite sprite, Predicate<AchievementsCheck> requirement) 
     {
         this.title = title;
         this.description = description;
@@ -145,8 +128,9 @@ public class Achievement
     public string description;
     public Sprite sprite;
 
+    public AchievementsCheck achievementsCheck;
 
-    public Predicate<object> requirement;
+    public Predicate<AchievementsCheck> requirement;
 
     public bool achieved;
 
@@ -169,7 +153,7 @@ public class Achievement
 
     public bool RequirementMet()
     {
-        return requirement.Invoke(null) && !achieved;
+        return requirement(achievementsCheck) && !achieved;
     }
 }
 
