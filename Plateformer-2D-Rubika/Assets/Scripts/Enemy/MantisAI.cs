b60967f626fa23fb560b49e64bc4c9381f318ecf
LeBud <charles.huyghe02@gmail.com>
2023-05-11 11:59:22 +0200

    MantisAI

--- Plateformer-2D-Rubika/Assets/Scripts/Enemy/MantisAI.cs
+++ Plateformer-2D-Rubika/Assets/Scripts/Enemy/MantisAI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class MantisAI : MonoBehaviour
 {
@@ -20,4 +21,77 @@ public class MantisAI : MonoBehaviour
     [SerializeField] bool followEnable = true;
     [SerializeField] bool jumpEnable = true;
     [SerializeField] bool directionLook = true;
+
+    Path path;
+    int currentWaypoint;
+    bool isGrounded;
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        seeker= GetComponent<Seeker>();
+        rb= GetComponent<Rigidbody2D>();
+
+
+        InvokeRepeating("UpdatePath", 0, pathUpdateSeconds);
+    }
+
+    void UpdatePath()
+    {
+        if(followEnable && TargetInDistance() && seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+    void PathFollow()
+    {
+        if (path == null)
+            return;
+
+        if (currentWaypoint >= path.vectorPath.Count)
+            return;
+
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, GetComponent<Collider2D>().bounds.extents.y + jumpCheckOffset);
+
+        Vector2 dir = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = dir * speed * Time.deltaTime;
+
+        if (jumpEnable && isGrounded)
+            if (dir.y > jumpNodeHeightRequirement)
+                rb.AddForce(Vector2.up * speed * jumpModifier);
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+        if (distance < nextWaypointDistance)
+            currentWaypoint++;
+
+
+        //Add changement de direction du sprite + cam
+    }
+
+    private void FixedUpdate()
+    {
+        if(TargetInDistance() && followEnable)
+        {
+            PathFollow();
+        }
+    }
+
+
+    bool TargetInDistance()
+    {
+        return Vector2.Distance(transform.position, target.position) < activateDistance;
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
 }
