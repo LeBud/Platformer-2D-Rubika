09a9b6ee1f2ce6a97bc9d4b47114dff265dd6ffe
axwls <119405498+axwls@users.noreply.github.com>
2023-06-06 16:32:55 +0200

    Revert "fg"
    This reverts commit d12b27acbfa1788c019e1bc8ef437a08fa7d89a8.

---
+++ Plateformer-2D-Rubika/Assets/Scripts/AxelleParralax.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxelleParralax : MonoBehaviour
+{
+
+    Transform    camTrans;
+    Vector3      lastCamPos;
+    float        textureUnitSize;
+
+    [SerializeField] float parallaxEffectMult = .1f;
+
+    private void Awake()
+    {
+        camTrans = Camera.main.transform;
+        lastCamPos = camTrans.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSize = texture.width / sprite.pixelsPerUnit;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 deltaMovement = camTrans.position - lastCamPos;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMult, 0, deltaMovement.z);
+
+        lastCamPos = camTrans.position;
+
+        /*if(Mathf.Abs(camTrans.position.x - transform.position.x) >= textureUnitSize)
+        {
+            float offsetPos = (camTrans.position.x - transform.position.x) % textureUnitSize;
+            transform.position = new Vector3(camTrans.position.x + offsetPos, transform.position.y, transform.position.z);
+        }*/
+    }
+
+}
